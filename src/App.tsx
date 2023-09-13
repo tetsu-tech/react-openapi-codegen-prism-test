f8b1329413c33fde305265417702580b2b52cff9
tetsu555 <tetete1118@gmail.com>
2023-09-13 12:21:16 +0900

    生成したクライアントを使ってみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useGetUsers } from "./tetsu/tetsuComponents";
+import { useGetUsers,  } from "./tetsu/tetsuComponents";
 
 const queryClient = new QueryClient();
 
